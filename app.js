391c8930c7bdf97572d87f05530c25de4f7670ca
Dillon Dipetrillo <Dillon.Dipetrillo@surepayroll.com>
2023-03-21 14:03:45 -0400

    updated js module pattern functions

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-const Gameboard = (() => {
+const Gameboard = () => {
+  // create empty array for board
+  const _gameboard = Array(9).fill("");
+
+  const getGameboard = () => _gameboard;
+
+  return {
+    getGameboard,
+  };
+};
+
+const Player = (name) => {
+  return { name };
+};
+
+const DisplayController = (() => {
   // cache DOM elements
   const _sqaures = document.querySelectorAll(".square");
 
-  const _gameboard = Array(9).fill("");
+  const board = Gameboard();
+
+  // create players
+  const playerX = Player("X");
+  const playerO = Player("O");
 
   // render a gameboard on DOM and add click event listeners
   const _renderBoard = () => {
     _sqaures.forEach((square, idx) => {
       square.addEventListener("click", _fillSquare);
-      square.textContent = _gameboard[idx];
+      square.textContent = board[idx];
     });
   };
 
   // Fill in square when clicked
-  const _fillSquare = (e) => {
+  const _fillSquare = (e, player) => {
     if (e.target.textContent !== "") {
       return;
     }
     const dataIndex = e.target.dataset.index;
-    _gameboard[dataIndex] = dataIndex;
+    board[dataIndex] = dataIndex;
     _renderBoard();
   };
 
